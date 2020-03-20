599f98d296ecdf6aade78267114015aa1c7718fb
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2020-03-20 18:16:59 +0500

    Functionality for "Add new item" and "Re-populate RecyclerView's List" has been successfully implemented.

--- app/src/main/java/org/abubaker/archexample/MainActivity.java
+++ app/src/main/java/org/abubaker/archexample/MainActivity.java
@@ -1,13 +1,18 @@
 package org.abubaker.archexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.abubaker.archexample.db.Note;
 import org.abubaker.archexample.vModel.NoteViewModel;
@@ -16,12 +21,26 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Constant: psfi
+    public static final int ADD_NOTE_REQUEST = 1;
+
     private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+        FloatingActionButton buttonAddNote = findViewById(R.id.btn_add_note);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Note: we cannot provide .this as a context, as it will refer to OnClickListener
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
+            }
+        });
 
         // Reference for our RecyclerView
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
@@ -40,6 +59,24 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setNotes(notes);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note = new Note(title, description, priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
